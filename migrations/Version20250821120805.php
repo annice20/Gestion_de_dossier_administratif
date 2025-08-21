28771b74a74bf53c7165dc492bef93f886939535
princy1312 <notahiana.princy@gmail.com>
2025-08-21 13:20:12 +0100

    modification

--- migrations/Version20250821120805.php
+++ migrations/Version20250821120805.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250821072437 extends AbstractMigration
+final class Version20250821120805 extends AbstractMigration
 {
     public function getDescription(): string
     {
