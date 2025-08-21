6ec3fcdacc7c7558f11f055ed1062b56283a8741
princy1312 <notahiana.princy@gmail.com>
2025-08-21 11:27:11 +0100

    piece jointe

---
+++ src/Form/AttachmentType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Attachment;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AttachmentType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+        ->add('type_piece', TextType::class, ['label' => 'Type de pièce'])
+        ->add('nom_fichier', TextType::class, ['label' => 'Nom du fichier'])
+        ->add('hash', TextType::class, ['label' => 'Hash'])
+        ->add('url', TextType::class, ['label' => 'URL'])
+        ->add('taille', IntegerType::class, ['label' => 'Taille'])
+        ->add('verif_statut', TextType::class, ['label' => 'Statut de vérification']);
+        
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Attachment::class,
+        ]);
+    }
+}
