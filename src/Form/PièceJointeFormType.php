adb139c131a960c77483f8339d254f9abb172d83
princy1312 <notahiana.princy@gmail.com>
2025-08-25 09:29:30 +0100

    dynamique piece jointe

--- src/Form/PièceJointeFormType.php
+++ src/Form/PièceJointeFormType.php
@@ -8,18 +8,46 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;  
 use Symfony\Component\Form\Extension\Core\Type\IntegerType; 
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;  
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;  
 
 class AttachmentType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('type_piece', TextType::class, ['label' => 'Type de pièce'])
+            ->add('type_piece', ChoiceType::class, [
+                'label' => 'Type de pièce',
+                'choices' => [
+                    'Carte d\'identité (CIN)' => 'CIN',
+                    'Passeport' => 'Passeport',
+                    'Permis de conduire' => 'Permis',
+                    'Acte de naissance' => 'Naissance',
+                    'Certificat de résidence' => 'Residence',
+                    'Carte électorale' => 'Electorale',
+                    'Carte professionnelle' => 'Professionnelle',
+                    'Carte d\'étudiant' => 'Etudiant',
+                    'Facture d\'électricité' => 'Facture_Electricite',
+                    'Facture d\'eau' => 'Facture_Eau',
+                    'Facture téléphonique' => 'Facture_Telephone',
+                    'Relevé bancaire' => 'Releve_Bancaire',
+                    'Bulletin de salaire' => 'Bulletin_Salaire',
+                    'Diplôme' => 'Diplome',
+                    'Attestation de travail' => 'Attestation_Travail',
+                    'Contrat de bail' => 'Contrat_Bail',
+                    'Quittance de loyer' => 'Quittance_Loyer',
+                    'Autre document' => 'Autre',
+                ],
+                'placeholder' => '--- Sélectionnez un type de pièce ---',
+            ])
             ->add('nom_fichier', TextType::class, ['label' => 'Nom du fichier'])
-            ->add('hash', TextType::class, ['label' => 'Hash'])
-            ->add('url', TextType::class, ['label' => 'URL'])
-            ->add('taille', IntegerType::class, ['label' => 'Taille'])
-            ->add('verif_statut', TextType::class, ['label' => 'Statut de vérification']);
+            ->add('hash', TextType::class, ['label' => 'Hash', 'required' => false])
+            ->add('url', TextType::class, ['label' => 'URL', 'required' => false])
+            ->add('taille', IntegerType::class, ['label' => 'Taille (en octets)', 'required' => false])
+            ->add('verif_statut', CheckboxType::class, [
+                'label' => 'Statut de vérification',
+                'required' => false
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
